b8d2b9062ecc3656d9cd18745c3968269763a3b9
WJ Lee <wjlee2122@gmail.com>
2021-05-07 19:08:55 +0900

    fire two dispatches for edit and closing

--- src/hooks/useEntryDetails.js
+++ src/hooks/useEntryDetails.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import { addEntryRedux, editEntryRedux } from "../redux/actions/entries.actions";
 import { v4 as uuidv4 } from 'uuid';
+import { closeModal } from "../redux/actions/modals.actions";
 
 export default function useEntryDetails(desc = "", val = "", isExp = true) {
     const [description, setDescription] = useState(desc);
@@ -36,6 +37,7 @@ export default function useEntryDetails(desc = "", val = "", isExp = true) {
                 isExpense
             }
         ))
+        dispatch(closeModal());
     }
 
     return {
