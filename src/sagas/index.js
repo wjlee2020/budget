5a6692a81de81bccdc83266200fe2f99f542aaf7
WJ Lee <wjlee2122@gmail.com>
2021-06-06 18:56:24 +0900

    testing for saga fork

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -1,7 +1,7 @@
-// import * as testSaga from './testSaga';
-import * as entriesSaga from './entriesSaga';
+import * as testSaga from './testSaga';
+// import * as entriesSaga from './entriesSaga';
 
 export function initSagas(sagaMiddleWare) {
-    // Object.values(testSaga).forEach(sagaMiddleWare.run.bind(sagaMiddleWare))
-    Object.values(entriesSaga).forEach(sagaMiddleWare.run.bind(sagaMiddleWare))
+    Object.values(testSaga).forEach(sagaMiddleWare.run.bind(sagaMiddleWare))
+    // Object.values(entriesSaga).forEach(sagaMiddleWare.run.bind(sagaMiddleWare))
 }
--- src/sagas/testSaga.js
+++ src/sagas/testSaga.js
@@ -1,4 +1,6 @@
-import { call, delay, put, take } from 'redux-saga/effects'
+// test sagas
+
+import { call, delay, fork, put, take } from 'redux-saga/effects'
 
 export function* testSaga() {
     while (true) {
@@ -13,10 +15,26 @@ export function* testSaga() {
     }
 }
 
+// function* doNothing() {
+//     console.log("do nothing pre!")
+//     yield delay(1000);
+//     console.log("i'm doing nothing")
+// }
+
+// export function* testSagaFork() {
+//     while (true) {
+//         yield take("TEST_MESSAGE_2")
+//      fork: non-blocking, following fn won't wait for fork to finish
+//         yield fork(doNothing);
+//         yield fork(doNothing);
+//         yield fork(doNothing);
+//     }
+// }
+
 // export function* dispatchTest() {
 //     while (true) {
-//         yield delay(1000);
-//         yield put({ type: "TEST_MESSAGE", payload: 1000 })
+//         yield delay(5000);
+//         yield put({ type: "TEST_MESSAGE_2", payload: 1000 })
 //     }
 // }
 
