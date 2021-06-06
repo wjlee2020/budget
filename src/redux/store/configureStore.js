aa9f635f274367fd27cca00bb5e57409351023cc
WJ Lee <wjlee2122@gmail.com>
2021-06-06 15:28:36 +0900

    loading up saga middleware

--- src/redux/store/configureStore.js
+++ src/redux/store/configureStore.js
@@ -1,13 +1,17 @@
-import { combineReducers, createStore } from "redux";
+import { applyMiddleware, combineReducers, createStore } from "redux";
 import { composeWithDevTools } from 'redux-devtools-extension';
 import entriesReducer from "../reducers/entries.reducer";
 import modalReducer from "../reducers/modalReducer";
+import createSagaMiddleWare from 'redux-saga';
+
+const sagaMiddleWare = createSagaMiddleWare();
+const middlewares = [sagaMiddleWare];
 
 const rootReducer = combineReducers({
     entries: entriesReducer,
     modals: modalReducer
 })
-const store = createStore(rootReducer, composeWithDevTools());
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(...middlewares)));
 store.subscribe(() => {
     console.log(store.getState());
 })
