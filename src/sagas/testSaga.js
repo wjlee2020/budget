b90059eeb8b736d49499aa9295bc73a45234a52a
WJ Lee <wjlee2122@gmail.com>
2021-06-19 14:22:49 +0900

    testing takeEvery Saga

--- src/sagas/testSaga.js
+++ src/sagas/testSaga.js
@@ -1,6 +1,6 @@
 // test sagas
 
-import { call, delay, fork, put, take } from 'redux-saga/effects'
+import { call, delay, fork, put, take, takeEvery } from 'redux-saga/effects'
 
 export function* testSaga() {
     while (true) {
@@ -15,28 +15,42 @@ export function* testSaga() {
     }
 }
 
-// function* doNothing() {
-//     console.log("do nothing pre!")
-//     yield delay(1000);
-//     console.log("i'm doing nothing")
-// }
+function* doNothing() {
+    console.log("do nothing pre!")
+    yield delay(1000);
+    console.log("i'm doing nothing")
+}
 
-// export function* testSagaFork() {
-//     while (true) {
-//         yield take("TEST_MESSAGE_2")
-//      fork: non-blocking, following fn won't wait for fork to finish (call waits for the result)
-//         yield fork(doNothing);
-//         yield fork(doNothing);
-//         yield fork(doNothing);
-//     }
-// }
+export function* testSagaFork() {
+    while (true) {
+        yield take("TEST_MESSAGE_2")
+        // fork: non - blocking, following fn won't wait for fork to finish (call waits for the result)
+        yield fork(doNothing);
+        yield fork(doNothing);
+        yield fork(doNothing);
+    }
+}
 
-// export function* dispatchTest() {
-//     while (true) {
-//         yield delay(5000);
-//         yield put({ type: "TEST_MESSAGE_2", payload: 1000 })
-//     }
-// }
+export function* testSagaTakeEveryProcess({ payload = 1 }) {
+    console.log(`Start process for index ${payload}`)
+    yield delay(3000);
+    console.log(`End process for index ${payload}`)
+}
+
+export function* testSagaTakeEvery() {
+    // takeEvery: getting many index, and if need to fetch each index use takeEvery
+    const { payload } = yield takeEvery("TEST_MESSAGE_3", testSagaTakeEveryProcess);
+    console.log("Finished TakeEvery for index" + " " + payload)
+}
+
+export function* dispatchTest() {
+    let index = 1;
+    while (true) {
+        yield delay(500);
+        yield put({ type: "TEST_MESSAGE_3", payload: index })
+        index++;
+    }
+}
 
 function double(num) {
     return num * 2;
